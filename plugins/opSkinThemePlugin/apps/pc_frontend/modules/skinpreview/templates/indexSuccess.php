169b76ee73ceace60e452304337f0d8bbcd0e8d1
手嶋守 <tejima@gmail.com>
2013-02-28 19:39:29 +0900

    remove

--- plugins/opSkinThemePlugin/apps/pc_frontend/modules/skinpreview/templates/indexSuccess.php
+++
@@ -1,7 +0,0 @@
-<?php if ($isExistsTheme): ?>
-  <p><?php echo __($themeName.'テーマをプレビュー表示しています') ?></p>
-<?php elseif ($emptyThemeName): ?>
-  <p><?php echo__('テーマ名のパラメーターが空です') ?></p>
-<?php else: ?>
-  <p><?php echo __($themeName.'テーマはありません') ?></p>
-<?php endif; ?>
--- plugins/opSkinThemePlugin/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$this->dispatcher->connect('op_action.post_execute', array('opThemeEvent', 'enableTheme'));
-$this->dispatcher->connect('op_action.post_execute', array('opThemeEvent', 'enablePreviewTheme'));
